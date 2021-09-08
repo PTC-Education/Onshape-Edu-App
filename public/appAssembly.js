749f19c6fd9937820e5823c3758a4565a78b8e6f
Matt Mueller <54808875+matthewlouismueller@users.noreply.github.com>
2021-09-08 11:15:14 -0400

    Json

--- public/appAssembly.js
+++ public/appAssembly.js
@@ -13,12 +13,12 @@ async function getMates() {
 };
 
 async function setMates(newJSON) {
-    var header =  {'Accept': 'application/vnd.onshape.v2+json',
-    'Content-Type': 'application/vnd.onshape.v2+json'};
+    var header =  {'Accept': 'application/json',
+    'Content-Type': 'application/json'};
     var raw = newJSON;
     console.log(raw);
     try {
-        const response = await fetch(`/api/setMateValues${window.location.search}`, {method: 'POST', body: raw, headers: header})
+        const response = await fetch(`/api/setMateValues${window.location.search}`, { method: 'POST', body: raw, headers: header})
         const matevalues = await response.json();
         return matevalues;
     } catch (error) {
