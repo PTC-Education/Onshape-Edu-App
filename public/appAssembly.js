5b54217d153cc377a2cdb8e18574eb6a0dd15316
Matt Mueller <54808875+matthewlouismueller@users.noreply.github.com>
2021-09-08 11:32:05 -0400

    json

--- public/appAssembly.js
+++ public/appAssembly.js
@@ -15,10 +15,7 @@ async function getMates() {
 async function setMates(newJSON) {
     var header =  {'Accept': 'application/json',
     'Content-Type': 'application/json'};
-    var raw = JSON.stringify({'mateValues':[{'featureId': "MTL9NFGQ/BOFQxUff",
-      'jsonType': "Revolute",
-      'mateName': "Revolute 1",
-      'rotationZ': 1}]});
+    var raw = JSON.stringify(newJSON);
     console.log(raw);
     try {
         const response = await fetch(`/api/setMateValues${window.location.search}`, { method: 'POST', body: raw, headers: header})
