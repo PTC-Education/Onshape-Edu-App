e2ac3febe8e75748c28d9f7c4d29faa9a0b16acf
Matt Mueller <54808875+matthewlouismueller@users.noreply.github.com>
2022-04-21 17:35:24 -0400

    again

--- app.js
+++ app.js
@@ -98,4 +98,8 @@ app.get('/gltf', (req, res) => {
 
 app.use('/api', require('./api'));
 
+app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')))
+app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')))
+
+
 module.exports = app;
